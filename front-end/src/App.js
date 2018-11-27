347cac6be65bbec774a0efe3c11c1209e29f074b
Reed-Anderson <reedineagan@comcast.net>
2018-11-26 21:21:37 -0600

    adding front end

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from './navbar';
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Navbar />
+      /* Body will go here */
+    );
+  }
+}
+
+export default App;
