324dadaa270984aca5dcd8d1f4ed86ac2d0ce53c
Braden Schroeder <braden14@viper.cs.ksu.edu>
2018-11-26 20:50:47 -0600

    Data File Added

---
+++ data.sql
@@ -0,0 +1,9 @@
+-- Work In Proggress!!!!!!!!!!!!!!
+
+insert into Demo.Feature ([Name])
+	values('Lift Kit'),('Tinted Windows'), ('Leather Seats'), ('Brake Kit'), ('Heated Seats'), ('Cooled Seats'), ('Backup Camera'), 
+	('Bluetooth'), ('Television'), ('Dual Televisions'), ('Heated Steering Wheel'), ('Upgraded Sound System'), ('Auto Pilot'), 
+	('Nitrus'), ('Off Road Tires'), ('OnStar'), ('Lane Assistance'), ('Four Wheel Drive'), ('Extended Cab'), ('Glass Packs')
+
+
+
