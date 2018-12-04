6a45c84704d1383d18541232b60951a003f50c81
Brent Krull <bkrull@WIN2.CS.KSU.EDU>
2018-12-03 20:35:17 -0600

    Add ListFeature to procedures, uncomment FeatureId's 6-20

--- procedures.sql
+++ procedures.sql
@@ -38,7 +38,7 @@ FROM Demo.Feature F
 WHERE CF.CarId = @CarId
 GO
 
-EXEC Demo.GetCarFeatures @CarId = 6;
+EXEC Demo.GetCarFeatures @CarId = 52;
 GO 
 
 /*---------------------------------------------------------------------------------
@@ -182,7 +182,7 @@ FROM
 WHERE C.CarId = @CarId
 GO
 
-EXEC Demo.CarInformation 652
+EXEC Demo.CarInformation 89
 
 /*---------------------------------------------------------------------------------
 Cars With X Features
@@ -196,24 +196,24 @@ CREATE PROCEDURE Demo.CarWithFeature
 	@Feature3 INT = null,
 	@Feature4 INT = null,
 	@Feature5 INT = null,
-	@Feature6 INT = null
-	--@Feature7 INT = null,
-	--@Feature8 INT = null,
-	--@Feature9 INT = null,
-	--@Feature10 INT = null,
-	--@Feature11 INT = null,
-	--@Feature12 INT = null,
-	--@Feature13 INT = null,
-	--@Feature14 INT = null,
-	--@Feature15 INT = null,
-	--@Feature16 INT = null,
-	--@Feature17 INT = null,
-	--@Feature18 INT = null,
-	--@Feature19 INT = null,
-	--@Feature20 INT = null
+	@Feature6 INT = null,
+	@Feature7 INT = null,
+	@Feature8 INT = null,
+	@Feature9 INT = null,
+	@Feature10 INT = null,
+	@Feature11 INT = null,
+	@Feature12 INT = null,
+	@Feature13 INT = null,
+	@Feature14 INT = null,
+	@Feature15 INT = null,
+	@Feature16 INT = null,
+	@Feature17 INT = null,
+	@Feature18 INT = null,
+	@Feature19 INT = null,
+	@Feature20 INT = null
 AS 
 
-SELECT C.CarId, C.Make, C.Model, C.Color, C.Milage--, CF.FeatureId
+SELECT C.CarId, C.Make, C.Model, C.[Year], C.AskPrice, C.Color, C.Milage, C.DealershipId, C.OwnerCnt
 FROM Demo.Car C
 	INNER JOIN Demo.CarFeature CF ON C.CarId = CF.CarId 
 WHERE C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId =ISNULL(@Feature1, CF.FeatureId) )
@@ -222,28 +222,41 @@ WHERE C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId =I
 	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature4, CF.FeatureId) )
 	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature5, CF.FeatureId) )
 	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature6, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature7, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature8, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature9, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature10, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature11, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature12, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature13, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature14, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature15, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature16, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature17, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature18, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature19, CF.FeatureId) )
-	--AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature20, CF.FeatureId) )
-GROUP BY C.CarId, C.Make, C.Model, C.Color, C.Milage
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature7, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature8, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature9, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature10, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature11, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature12, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature13, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature14, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature15, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature16, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature17, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature18, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature19, CF.FeatureId) )
+	AND C.CarId IN ( SELECT CF.CarId FROM Demo.CarFeature CF WHERE CF.FeatureId = ISNULL(@Feature20, CF.FeatureId) )
+GROUP BY C.CarId, C.Make, C.Model, C.[Year], C.AskPrice, C.Color, C.Milage, C.DealershipId, C.OwnerCnt
 GO
 
 EXEC Demo.CarWithFeature 
-	@Feature1 = 2,
-	@Feature2 = 7,
+	@Feature1 = 20,
+	@Feature2 = 19,
 	@Feature3 = 8;
 GO
+
+/*---------------------------------------------------------------------------------
+Get all features
+---------------------------------------------------------------------------------*/
+DROP PROCEDURE IF EXISTS Demo.ListFeature;
+GO
+
+CREATE PROCEDURE Demo.ListFeature
+AS
+SELECT F.[Name], F.FeatureId
+FROM Demo.Feature F;
+
+EXEC Demo.ListFeature;
 
 /*---------------------------------------------------------------------------------
 Get Employees - From Dealership (Duplicate of above?)
